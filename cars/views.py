c6ec5649950c01f8b02bc5336a22009b183af58f
Julio Gazoli <julio_gazoli@hotmail.com>
2023-11-03 15:24:27 -0300

    configurado views e urls

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+
+def cars_view(request):
+    html = '''
+        <html>
+            <head>
+                <title>Meus Carros</title>
+            </head>
+            <body>
+                <h1>Carros da PycodeBR</h1>
+                <h3>Só carro top!</h3>
+            </body>
+        </html>
+    '''
+    return HttpResponse(html)
