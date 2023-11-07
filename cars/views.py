889f521e304e56363ec633cbd4ab897cec996c82
Julio Gazoli <julio_gazoli@hotmail.com>
2023-11-07 16:21:26 -0300

    adicionado template cars

--- cars/views.py
+++ cars/views.py
@@ -1,16 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 
 def cars_view(request):
-    html = '''
-        <html>
-            <head>
-                <title>Meus Carros</title>
-            </head>
-            <body>
-                <h1>Carros da PycodeBR</h1>
-                <h3>Só carro top!</h3>
-            </body>
-        </html>
-    '''
-    return HttpResponse(html)
+    return render(request, 'cars.html', {'cars': {'model': 'Astra 2.0'}})
