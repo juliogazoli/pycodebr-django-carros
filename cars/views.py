fddde0133e77e036355df339c49223670d2e2060
Julio Gazoli <julio_gazoli@hotmail.com>
2023-11-11 11:11:05 -0300

    django orm

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from cars.models import Car
 
 
 def cars_view(request):
-    return render(request, 'cars.html', {'cars': {'model': 'Astra 2.0'}})
+    cars = Car.objects.all().order_by('model')
+
+    search = request.GET.get('search')
+
+    if search:
+        cars = cars.filter(model__icontains=search)
+
+    return render(request, 'cars.html', {'cars': cars})
