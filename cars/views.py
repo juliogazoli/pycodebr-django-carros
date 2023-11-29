c9664772c27eb76479c638c0713a293659d315ce
Julio Gazoli <julio_gazoli@hotmail.com>
2023-11-29 17:29:17 -0300

    adicionado formulário de cadastro de carros

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Car
-
+from cars.forms import CarModelForm
 
 def cars_view(request):
     cars = Car.objects.all().order_by('model')
@@ -11,3 +11,14 @@ def cars_view(request):
         cars = cars.filter(model__icontains=search)
 
     return render(request, 'cars.html', {'cars': cars})
+
+
+def new_car_view(request):
+    if request.method == 'POST':
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarModelForm()
+    return render(request, 'new_car.html', {'new_car_form': new_car_form})
